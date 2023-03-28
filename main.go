b3cd684e1a4974371f589e0ed19af56e27783d60
beezyfbaby <25128440+beezyfbaby@users.noreply.github.com>
2023-03-28 13:43:57 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,85 +1,78 @@
-package main
+package instacart
 
 import (
-	"encoding/csv"
 	"log"
-	"os"
-	"strconv"
-	"strings"
+	"sort"
 	"time"
-
-	instacart "github.com/beezyfbaby/go-instacart-export"
 )
 
-func main() {
-	sessionToken := os.Getenv("INSTACART_SESSION_TOKEN")
+const timeFormat = "Jan 2, 2006,  3:04 PM"
 
-	if sessionToken == "" {
-		log.Fatal("Session token missing. Please provide the INSTACART_SESSION_TOKEN environment variable")
+func extractOrders(orderResp OrdersResponse) []*Order {
+	var orders []*Order
+	for _, o := range orderResp.Orders {
+		order := &Order{}
+		order.ID = o.ID
+		order.Status = o.Status
+		order.Total = o.Total
+
+		createdAt, err := time.Parse(timeFormat, o.CreatedAt)
+		if err != nil {
+			log.Fatalf("Unable to parse order time: %s | %v", o.CreatedAt, err)
+		}
+		order.CreatedAt = createdAt
+
+		var deliveries []*Delivery
+		for _, d := range o.OrderDeliveries {
+			delivery := &Delivery{}
+			delivery.Retailer = d.Retailer.Name
+
+			if d.DeliveredAt != "" {
+				deliveredAt, err := time.Parse(timeFormat, d.DeliveredAt)
+				if err != nil {
+					log.Fatalf("Unable to parse delivery time: %s | %v", d.DeliveredAt, err)
+				}
+				delivery.DeliveredAt = deliveredAt
+			}
+
+			var items []*Item
+			for _, i := range d.OrderItems {
+				item := &Item{}
+				item.ID = i.Item.ID
+				item.ProductID = i.Item.ProductID
+				item.Quantity = int(i.Qty)
+				item.Name = i.Item.Name
+
+				items = append(items, item)
+			}
+
+			delivery.Items = items
+			deliveries = append(deliveries, delivery)
+		}
+
+		order.Deliveries = deliveries
+		orders = append(orders, order)
 	}
 
-	client := instacart.Client{
-		SessionToken: sessionToken,
-	}
-
-	log.Print("Fetching orders...")
-	orders := instacart.FetchOrders(client)
-	data := extractOrdersData(orders)
-	writeToCSV(data)
-
-	log.Print("Done!")
+	return orders
 }
 
-func extractOrdersData(orders []*instacart.Order) [][]string {
-	log.Print("Processing orders")
-	data := [][]string{{
-		"id",
-		"status",
-		"total",
-		"createdAt",
-		"retailers",
-		"numItems",
-	}}
-	for _, o := range orders {
+// FetchOrders retrieves all orders sorted by date created, descending.
+func FetchOrders(client Client) []*Order {
+	var orders []*Order
+	var resp OrdersResponse
+	var nextPage *int
+	nextPage = new(int)
 
-		var retailers []string
-		numItems := 0
+	*nextPage = 1
 
-		for _, d := range o.Deliveries {
-			retailers = append(retailers, d.Retailer)
-			numItems += len(d.Items)
-		}
-
-		order := []string{
-			o.ID,
-			o.Status,
-			o.Total,
-			o.CreatedAt.Format("2006-01-02"),
-			strings.Join(retailers, "|"),
-			strconv.Itoa(numItems),
-		}
-		data = append(data, order)
+	for nextPage != nil {
+		log.Printf("Fetching page: %d", *nextPage)
+		resp = client.getPage(*nextPage)
+		orders = append(orders, extractOrders(resp)...)
+		nextPage = resp.Meta.Pagination.NextPage
 	}
 
-	return data
-}
-
-func writeToCSV(data [][]string) {
-	log.Print("Writing orders to a CSV")
-
-	now := time.Now()
-	file, err := os.Create("data/instacart_orders_" + now.Format("01-02-2006_03-04-05") + ".csv")
-	if err != nil {
-		log.Fatal("Unable to create file", err)
-	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	for _, row := range data {
-		if err := writer.Write(row); err != nil {
-			log.Fatal("Error writing data", err)
-		}
-	}
+	sort.Sort(sortOrderByDate{orders})
+	return orders
 }

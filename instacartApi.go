455901a1e3c50147afa72aa5a03dafd25103b847
beezyfbaby <25128440+beezyfbaby@users.noreply.github.com>
2023-12-31 17:52:57 -0500

    Update instacartApi.go

--- instacartApi.go
+++ instacartApi.go
@@ -93,7 +93,7 @@ func (c *Client) getPage(page int) OrdersResponse {
 type OrdersResponse struct {
 	Orders []struct {
 		ID        string  `json:"id"`
-		LegacyID  int     `json:"legacy_id,string"`
+		LegacyID  int     `json:"legacy_id"`
 		Status    string  `json:"status"`
 		Rating    float32 `json:"rating"`
 		Total     string  `json:"total"`
